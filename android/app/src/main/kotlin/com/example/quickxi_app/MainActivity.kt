27927c256472aa28ddb86aa8c0d985a32d281630
gyuminv2 <inamemin3@gmail.com>
2024-05-05 20:44:04 +0900

    new proj

--- android/app/src/main/kotlin/com/example/quickxi_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quickxi_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.capstone.quickxi_app
+package com.example.quickxi_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-
-import 'Screens/employeeDrawer.dart';
-// import 'src/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+/// stless -> StatelessWidget 생성 단축키
+/// StatelessWidget(HomeScreen)
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      title: 'EmployeeManagement',debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: true,
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/taxi.png',
+          ),
+          Text(
+            'Quickxi',
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '세상에 없던 혁신적인 어쩌고 퀵 배송 저쩌고',
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('로그인 버튼 클릭');
+            },
+            child: Text('로그인'),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('회원가입 버튼 클릭');
+            },
+            child: Text('회원가입'),
+          )
+        ],
       ),
-      home: employeeDrawer(),
     );
   }
 }
