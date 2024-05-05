76c5ead282461482a2e8d29ec42109e13dcd36b4
홍한울 <honghan3235@gmail.com>
2024-05-06 07:37:17 +0900

    fail

--- lib/map.dart
+++ lib/map.dart
@@ -15,10 +15,10 @@ class MapMain extends StatefulWidget {
 class _MapMainState extends State<MapMain> {
   final locationController = Location();
 
-  static const sourLocation = LatLng(37.282470, 127.900079);
-  static const destLocation = LatLng(37.280129, 127.9120728);
+  static const sourLocation = LatLng(37.339905, 127.937990);
+  static const destLocation = LatLng(37.347464, 127.954386);
 
-  LatLng? currentPosition;
+  LatLng? currentPosition= LatLng(37.282470, 127.900079);
   Map<PolylineId, Polyline> polylines = {};
 
   @override
@@ -36,12 +36,21 @@ class _MapMainState extends State<MapMain> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: currentPosition == null
+    appBar: AppBar(
+      title: const Text('Quicxi',
+        style: TextStyle(color: Colors.white),),
+      backgroundColor: const Color.fromARGB(255, 0, 68, 2),
+    ),
+    body:Column(
+      children: [
+      currentPosition == null
         ? const Center(child: CircularProgressIndicator())
-        : GoogleMap(
+        :Container(
+      height: 450,
+      child: GoogleMap(
       initialCameraPosition: const CameraPosition(
         target: sourLocation,
-        zoom: 13,
+        zoom: 15,
       ),
       markers: {
         Marker(
@@ -62,6 +71,87 @@ class _MapMainState extends State<MapMain> {
       },
       polylines: Set<Polyline>.of(polylines.values),
     ),
+    ),
+        Expanded(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(
+
+                color: const Color.fromARGB(255, 0, 68, 2),
+                width:40,
+              ),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 13),
+                alignment: Alignment.centerLeft,
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: '출발지와 도착지를\n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,), // 굵은 텍스트로 설정합니다.
+                      ),
+                      TextSpan(
+                        text: '입력해주세요.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,), // 이탤릭체 텍스트로 설정합니다.
+                      ),
+                    ],
+                   ),
+                  ),
+                ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '현위치',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                style: TextStyle(color: Colors.grey),
+                // 출발지를 입력받는 함수 또는 변수 지정
+                //onChanged: (value1) {
+                  // 출발지가 변경될 때마다 이벤트를 처리할 수 있습니다.
+                  // 변경된 값은 value 매개변수를 통해 전달됩니다.
+                  // 여기에 출발지를 저장하거나 처리하는 코드를 작성할 수 있습니다.
+               // },
+              ),
+              SizedBox(height: 10), // 공간을 위해 SizedBox 추가
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '도착지',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                style: TextStyle(color: Colors.grey),
+                // 도착지를 입력받는 함수 또는 변수 지정
+                //onChanged: (value2) {
+                  // 도착지가 변경될 때마다 이벤트를 처리할 수 있습니다.
+                  // 변경된 값은 value 매개변수를 통해 전달됩니다.
+                  // 여기에 도착지를 저장하거나 처리하는 코드를 작성할 수 있습니다.
+                //},
+              ),
+            ],
+          ),
+        ),
+        ),
+    ],
+    ),
   );
 
   Future<void> fetchLocationUpdates() async {
@@ -72,7 +162,7 @@ class _MapMainState extends State<MapMain> {
     if (serviceEnabled) {
       serviceEnabled = await locationController.requestService();
     } else {
-      return;
+      return ;
     }
 
     permissionGranted = await locationController.hasPermission();
@@ -123,7 +213,7 @@ class _MapMainState extends State<MapMain> {
       polylineId: id,
       color: Colors.blueAccent,
       points: polylineCoordinates,
-      width: 5,
+      width: 2,
     );
 
     setState(() => polylines[id] = polyline);
